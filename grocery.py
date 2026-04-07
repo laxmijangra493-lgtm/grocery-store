6963f5acc66fdba0448cb4f4ed08e2bc65b03ad4
laxmijangra493-lgtm <laxmijangra493@gmail.com>
2026-04-06 16:31:30 -1200

    Update grocery.py

--- grocery.py
+++ grocery.py
@@ -87,7 +87,6 @@ def view_cart():
     for item in items:
         print(f"ID: {item[0]} | {item[1]} - ₹{item[2]}")
     pay = input("enter 'pay' to pay your bill & '2' for continue: ")
-    total = 0
     if pay == "pay":
             total =0 
             for item in items:
